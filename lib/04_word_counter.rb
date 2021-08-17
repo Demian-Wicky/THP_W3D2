fff8a2978c35b2be0301c3cba0ca66a38f397b16
Demian-Wicky <demian.wicky@gmail.com>
2021-08-17 18:38:52 +0200

    04_word_counter OK

---
+++ lib/04_word_counter.rb
@@ -0,0 +1,22 @@
+dictionnary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+
+class Hash
+  def compact
+    delete_if { |k, v| v.nil? }
+  end
+end
+
+def word_counter(string, dictionnary)
+  string = string.downcase
+  h = Hash.new
+  dictionnary.each do |word|
+    if string.scan(/(?=#{word})/).count != 0
+      h[word] = string.scan(/(?=#{word})/).count
+    end
+  end
+return h.compact
+end
+
+
+print word_counter("Howdy partner, sit down! How's it going?", dictionnary)
+
